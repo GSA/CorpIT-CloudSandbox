b56bc09ce15a13dacb14bd84472a78fa5a1b7f3a
ryan.day <ryan.day@gsa.gov>
2017-09-08 09:59:52 -0500

    Initial push of sample API

---
+++ api/Startup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using api.Models;
+
+namespace api
+{
+    public class Startup
+    {       
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AccessRequestContext>(opt => opt.UseInMemoryDatabase("AccessRequestList"));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
